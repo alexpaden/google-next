48d9c4d8b28fde9ed21429768610f66b81ca61e2
Alex Paden <padenfool@gmail.com>
2023-01-03 18:09:40 -0600

    Basic Google.com Homepage
    Footer component not included

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Avatar from '../components/Avatar'
+import { SearchIcon, MicrophoneIcon, ViewGridIcon } from '@heroicons/react/solid'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -22,16 +24,33 @@ export default function Home() {
       <div className="flex space-x-4 items-center">
         <p className="link">Gmail</p>
         <p className="link">Images</p>
+
+        {/*Icon */}
+        <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
+
+        {/* Avatar */}
+        <Avatar url="https://links.papareact.com/5me" />
       </div>
-
-      {/*Icon */}
-
-      {/* Avatar */}
-      <Avatar url="https://links.papareact.com/5me" />
       </header>
 
       {/* Body */}
+        <form className="flex flex-col items-center mt-44">
+          <Image
+            src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+            height={100}
+            width={300}
+          />
+          <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+            <SearchIcon className="h-5 mr-3 text-gray-500" />
+            <input type="text" className="flex-grow focus:outline-none" />
+            <MicrophoneIcon className="h-5" />
+          </div>
 
-    </div>
+          <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+            <button className="btn">Google Search</button>
+            <button className="btn">I'm Feeling Lucky</button>
+          </div>
+        </form>
+      </div>
   )
 }
